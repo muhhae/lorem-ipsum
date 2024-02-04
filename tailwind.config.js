b53878f62682628a369949604c1e42eae02415f8
muhhae <haekal.muhamat@gmail.com>
2024-02-04 11:19:58 +0700

    refactor : delete unnecessary line

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./**/*.templ"
-  ],
-  theme: {
-    extend: {
-      backgroundImage: theme => ({
-        'gradient-to-l': `linear-gradient(to left, ${theme('colors.primary.DEFAULT')} 0%, transparent 100%)`
-      })
-    }
-  },
-  daisyui: {
-    themes: ["nord", "sunset"],
-  },
-  plugins: [require("daisyui")],
+    content: [
+        "./pkg/template/*.templ",
+        "./pkg/component/*.templ",
+        "./internal/views/**/*.templ"
+    ],
+    daisyui: {
+        themes: ["nord", "sunset"],
+    },
+    plugins: [require("daisyui")],
 }
 
