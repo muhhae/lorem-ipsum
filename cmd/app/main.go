436e142909f516f0bc2a3a9633a2ce239d9dac5a
muhhae <haekal.muhamat@gmail.com>
2024-03-04 16:11:56 +0700

    docker compose

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/muhhae/lorem-ipsum/internal"
 )
 
 func main() {
+	fmt.Println("This is edit docker image")
+	fmt.Println("This is another new line")
 	internal.InitAll()
 }
