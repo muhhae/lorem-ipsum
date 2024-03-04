fa1108862a4866728b9cf9d529b43ec0be0fb072
muhhae <haekal.muhamat@gmail.com>
2024-03-04 16:43:44 +0700

    testing another workflow

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/muhhae/lorem-ipsum/internal"
 )
 
 func main() {
-	fmt.Println("This is edit docker image")
-	fmt.Println("This is another new line")
 	internal.InitAll()
 }
